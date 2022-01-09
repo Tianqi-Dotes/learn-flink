c22711f2b666b7b8f72400ea28672313c6374c61
Tq <tianqitobethefirst@gmail.com>
2022-01-09 22:18:55 +0800

    ParallelSourceFunction和SourceFunction的实现

--- datastream/src/main/java/com/tq/datastream/DataStreamTest.java
+++ datastream/src/main/java/com/tq/datastream/DataStreamTest.java
@@ -1,5 +1,8 @@
 package com.tq.datastream;
 
+import com.tq.sink.AccessSource;
+import com.tq.sink.AccessSourceMulti;
+import com.tq.transformation.Access;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -18,14 +21,35 @@ public class DataStreamTest {
         //创建上下文
         StreamExecutionEnvironment en=StreamExecutionEnvironment.getExecutionEnvironment();
 
-        test03(en);
+        //test03(en);
+        //test04(en);
+        test05(en);
         en.execute("DataStreamTest");
     }
 
+    //多线程source
+    public static void test05(StreamExecutionEnvironment en){
+
+        //source 并行度默认为cpu线程数
+        DataStreamSource<Access> accessDataStreamSource = en.addSource(new AccessSourceMulti()).setParallelism(16);
+        System.out.println(accessDataStreamSource.getParallelism());
+        accessDataStreamSource.print();
+    }
+    //单线程source
+    public static void test04(StreamExecutionEnvironment en){
+
+        //source 并行度默认为1
+        DataStreamSource<Access> accessDataStreamSource = en.addSource(new AccessSource());
+        System.out.println(accessDataStreamSource.getParallelism());
+        accessDataStreamSource.print();
+    }
+
+    //接入kafka
     public static void test03(StreamExecutionEnvironment en) {
         Properties properties=new Properties();
         properties.setProperty("bootstrap.servers","localhost:9092");
         properties.setProperty("group_id","test");
+        //接入源
         DataStreamSource<String> kafka = en.addSource(new FlinkKafkaConsumer<>("flink", new SimpleStringSchema(), properties));
         System.out.println(kafka.getParallelism());
         kafka.print();
---
+++ datastream/src/main/java/com/tq/sink/AccessSourceMulti.java
@@ -0,0 +1,37 @@
+package com.tq.sink;
+
+import com.tq.transformation.Access;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import java.util.Random;
+
+public class AccessSourceMulti implements ParallelSourceFunction<Access> {
+
+    boolean running=true;
+
+    @Override
+    public void run(SourceContext<Access> ctx) throws Exception {
+
+        String[] websites={"tq.com","a.com","b.com"};
+        Random random = new Random();
+
+        while (running) {
+            for (int i = 0; i < 10; i++) {
+                Access access = new Access();
+                access.setConsume(random.nextInt(2000));
+                access.setDate(123456L);
+                access.setWebsite(websites[random.nextInt(websites.length)]);
+
+
+                ctx.collect(access);
+            }
+            Thread.sleep(3000);
+        }
+    }
+
+    @Override
+    public void cancel() {
+        running=false;
+    }
+}
