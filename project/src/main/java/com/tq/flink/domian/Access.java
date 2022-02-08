e0d35c270e1b64cacff003f8cbcbfba77360acb8
Tq <tianqitobethefirst@gmail.com>
2022-02-08 17:20:00 +0800

    添加新项目以及access.json类的提交

---
+++ project/src/main/java/com/tq/flink/domian/Access.java
@@ -0,0 +1,27 @@
+package com.tq.flink.domian;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Access {
+
+    public String device;
+    public String deviceType;
+    public String os;
+    public String event;
+    public String net;
+    public String channel;
+    public String uid;
+    public Integer nu;  // 1新
+    public Integer nu2;
+    public String ip;  // ==> ip去解析
+    public Long time;
+    public String version;
+    public String province;
+    public String city;
+
+    public Product product;
+
+}
---
+++ project/src/main/java/com/tq/flink/domian/Product.java
@@ -0,0 +1,13 @@
+package com.tq.flink.domian;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Product {
+
+    public String category;
+    public String name;
+
+}
